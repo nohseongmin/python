afd9f53bad4dcbcb34e82c848cdee306784113a4
nohseongmin <sm9375693@gmail.com>
2025-04-17 00:00:54 +0900

    4/17

---
+++ baekjoon/16430.py
@@ -0,0 +1,3 @@
+#백준 16430 제리와 톰
+a, b= map(int, input().split(" "))
+print(b-a, b)
---
+++ baekjoon/5717.py
@@ -0,0 +1,6 @@
+#백준 5717 상근이의 친구들
+while True:
+    a,b = input().split(" ")
+    if int(a)==0 and int(b)==0:
+        break
+    print(int(a)+int(b))
